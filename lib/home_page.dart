1ab718fbc852d93209f1b889527c6332dea69757
fidelis <fidelisnwabisi@gmail.com>
2023-07-12 13:18:12 +0100

    Connected UI and database

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'app_state.dart';
+import 'guest_book.dart';
 import 'src/authentication.dart';
 import 'src/widgets.dart';
 
@@ -45,17 +46,22 @@ class HomePage extends StatelessWidget {
           const Paragraph(
             'Join us for a day full of Firebase Workshops and Pizza!',
           ),
-          const Header("Discussion"),
-          GuestBook(addMessage: (String message) => print(message)),
-
-          // Consumer<ApplicationState>(
-          //   builder: (context, appState, _) => Column(
-          //     crossAxisAlignment: CrossAxisAlignment.start,
-          //     children: [
-          //       if (appState.loggedIn) ...[const Header("heading")]
-          //     ],
-          //   ),
-          // ),
+          // const Header("Discussion"),
+          // GuestBook(addMessage: (String message) => print(message)),
+          Consumer<ApplicationState>(
+            builder: (context, appState, _) => Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                if (appState.loggedIn) ...[
+                  const Header("Discussion"),
+                  GuestBook(
+                    addMessage: (message) =>
+                        appState.addMessageToGuestBook(message),
+                  ),
+                ]
+              ],
+            ),
+          ),
         ],
       ),
     );
